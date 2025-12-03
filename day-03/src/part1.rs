d2b0ab35aeb9b5ea6a3f758a8a0f00045b859a2b
mrkiffie <mrkiffie@gmail.com>
2025-12-03 09:33:39 +0200

    Solve day-03 part1

--- day-03/src/part1.rs
+++ day-03/src/part1.rs
@@ -6,17 +6,38 @@ pub fn main() {
 }
 
 fn solve(input: &[u8]) -> i32 {
-    0
+    input.split(|c| *c == b'\n').map(find_joltage).sum()
 }
-
+fn find_joltage(input: &[u8]) -> i32 {
+    let mut first = b'0';
+    let mut second = b'0';
+    for d in input {
+        if second > first {
+            first = second;
+            second = b'0';
+        }
+        if *d > second {
+            second = *d;
+        }
+    }
+    i32::from(first - b'0') * 10 + i32::from(second - b'0')
+}
 #[cfg(test)]
 mod tests {
-    use super::solve;
+    use super::{find_joltage, solve};
 
     #[test]
-    fn test_1() {
-        let result = solve(b"");
-        assert_eq!(result, 0);
+    fn test_find_joltage() {
+        assert_eq!(find_joltage(b"987654321111111"), 98);
+        assert_eq!(find_joltage(b"811111111111119"), 89);
+        assert_eq!(find_joltage(b"234234234234278"), 78);
+        assert_eq!(find_joltage(b"818181911112111"), 92);
+    }
+
+    #[test]
+    fn test_solve() {
+        let result = solve(b"987654321111111\n811111111111119\n234234234234278\n818181911112111");
+        assert_eq!(result, 357);
     }
 }
 
