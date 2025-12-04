e626cf076c9b319c7fb24ee9024d03c36c463849
mrkiffie <mrkiffie@gmail.com>
2025-12-04 07:48:47 +0200

    Solve day-04 part2

--- day-04/src/part2.rs
+++ day-04/src/part2.rs
@@ -1,12 +1,65 @@
-const INPUT: &[u8] = include_bytes!("./input.txt");
+use glam::IVec2;
+use std::collections::HashSet;
+
+const INPUT: &str = include_str!("./input.txt");
 
 pub fn main() {
     let result = solve(INPUT);
     println!("{result}");
 }
 
-fn solve(input: &[u8]) -> i32 {
-    0
+fn solve(input: &str) -> usize {
+    let mut lookup = parse_input(input);
+    let mut total: usize = 0;
+    let mut queue = Vec::with_capacity(2048);
+    loop {
+        for roll in &lookup {
+            let count = [
+                IVec2::new(-1, -1),
+                IVec2::new(0, -1),
+                IVec2::new(1, -1),
+                IVec2::new(-1, 0),
+                IVec2::new(1, 0),
+                IVec2::new(-1, 1),
+                IVec2::new(0, 1),
+                IVec2::new(1, 1),
+            ]
+            .iter()
+            .filter(|n| lookup.contains(&(roll + *n)))
+            .count();
+
+            if count < 4 {
+                queue.push(*roll);
+            }
+        }
+
+        if queue.is_empty() {
+            return total;
+        }
+
+        for roll in &queue {
+            lookup.remove(roll);
+        }
+        total += queue.len();
+        queue.clear();
+    }
+}
+
+fn parse_input(input: &str) -> HashSet<IVec2> {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().filter_map(move |(x, c)| {
+                if c == '@' {
+                    #[allow(clippy::cast_possible_wrap, clippy::cast_possible_truncation)]
+                    Some(IVec2::new(x as i32, y as i32))
+                } else {
+                    None
+                }
+            })
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -15,8 +68,10 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let result = solve(b"");
-        assert_eq!(result, 0);
+        let result = solve(
+            "..@@.@@@@.\n@@@.@.@.@@\n@@@@@.@.@@\n@.@@@@..@.\n@@.@@@@.@@\n.@@@@@@@.@\n.@.@.@.@@@\n@.@@@.@@@@\n.@@@@@@@@.\n@.@.@@@.@.",
+        );
+        assert_eq!(result, 43);
     }
 }
 
@@ -31,3 +86,4 @@ pub mod benchmarks {
         super::solve(INPUT);
     }
 }
+
