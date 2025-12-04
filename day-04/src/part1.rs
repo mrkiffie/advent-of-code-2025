152dccf4e8fe7a43df9ae6e4c2d8bfe6b73d1cdb
mrkiffie <mrkiffie@gmail.com>
2025-12-04 07:30:34 +0200

    Solve day-04 part1

--- day-04/src/part1.rs
+++ day-04/src/part1.rs
@@ -1,12 +1,53 @@
-const INPUT: &[u8] = include_bytes!("./input.txt");
+use glam::IVec2;
+use std::collections::HashSet;
+
+const INPUT: &str = include_str!("./input.txt");
 
 pub fn main() {
     let result = solve(INPUT);
     println!("{result}");
 }
 
-fn solve(input: &[u8]) -> i32 {
-    0
+fn solve(input: &str) -> i32 {
+    let lookup = parse_input(input);
+    let mut total = 0;
+    for roll in &lookup {
+        let count = [
+            IVec2::new(-1, -1),
+            IVec2::new(0, -1),
+            IVec2::new(1, -1),
+            IVec2::new(-1, 0),
+            IVec2::new(1, 0),
+            IVec2::new(-1, 1),
+            IVec2::new(0, 1),
+            IVec2::new(1, 1),
+        ]
+        .iter()
+        .filter(|n| lookup.contains(&(roll + *n)))
+        .count();
+
+        if count < 4 {
+            total += 1;
+        }
+    }
+    total
+}
+
+fn parse_input(input: &str) -> HashSet<IVec2> {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().filter_map(move |(x, c)| {
+                if c == '@' {
+                    #[allow(clippy::cast_possible_wrap, clippy::cast_possible_truncation)]
+                    Some(IVec2::new(x as i32, y as i32))
+                } else {
+                    None
+                }
+            })
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -15,8 +56,10 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let result = solve(b"");
-        assert_eq!(result, 0);
+        let result = solve(
+            "..@@.@@@@.\n@@@.@.@.@@\n@@@@@.@.@@\n@.@@@@..@.\n@@.@@@@.@@\n.@@@@@@@.@\n.@.@.@.@@@\n@.@@@.@@@@\n.@@@@@@@@.\n@.@.@@@.@.",
+        );
+        assert_eq!(result, 13);
     }
 }
 
