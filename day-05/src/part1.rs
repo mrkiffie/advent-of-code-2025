bc12d7c7b2162088db4985220536273931d6040c
mrkiffie <mrkiffie@gmail.com>
2025-12-05 08:52:21 +0200

    Solve day-05 part1

--- day-05/src/part1.rs
+++ day-05/src/part1.rs
@@ -1,12 +1,32 @@
-const INPUT: &[u8] = include_bytes!("./input.txt");
+const INPUT: &str = include_str!("./input.txt");
 
 pub fn main() {
     let result = solve(INPUT);
     println!("{result}");
 }
 
-fn solve(input: &[u8]) -> i32 {
-    0
+fn solve(input: &str) -> usize {
+    let mut inventory = vec![];
+    let mut fresh_ranges = vec![];
+    let mut blank_lines = false;
+    for line in input.trim().lines() {
+        if line.is_empty() {
+            blank_lines = true;
+        } else if blank_lines {
+            inventory.push(line.parse::<u64>().unwrap());
+        } else if let Some((low, high)) = line.split_once('-') {
+            let low = low.parse::<u64>().unwrap();
+            let high = high.parse::<u64>().unwrap();
+            fresh_ranges.push(low..=high);
+        } else {
+            unreachable!()
+        }
+    }
+
+    inventory
+        .iter()
+        .filter(|id| fresh_ranges.iter().any(|range| range.contains(*id)))
+        .count()
 }
 
 #[cfg(test)]
@@ -15,8 +35,8 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let result = solve(b"");
-        assert_eq!(result, 0);
+        let result = solve("3-5\n10-14\n16-20\n12-18\n\n1\n5\n8\n11\n17\n32");
+        assert_eq!(result, 3);
     }
 }
 
