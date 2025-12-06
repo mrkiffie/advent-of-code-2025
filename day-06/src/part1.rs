71f4b37a1c384fb205fb82d929a4e5ad988a615a
mrkiffie <mrkiffie@gmail.com>
2025-12-06 13:02:29 +0200

    Solve day-06 part1

--- day-06/src/part1.rs
+++ day-06/src/part1.rs
@@ -5,8 +5,77 @@ pub fn main() {
     println!("{result}");
 }
 
-fn solve(input: &[u8]) -> i32 {
-    0
+fn solve(input: &[u8]) -> u64 {
+    let row_length = input.iter().position(|c| *c == b'\n').unwrap();
+    let number_of_rows = input.len() / row_length;
+
+    let operator_index = (number_of_rows - 1) * (row_length + 1);
+    let ops = &input[operator_index..operator_index + row_length];
+
+    let mut grand_total = 0;
+
+    let mut numbers = [0; 4];
+    let mut op = b' ';
+
+    for x in 0..row_length {
+        let mut found_digit = false;
+        for y in 0..number_of_rows - 1 {
+            let c = input[(row_length + 1) * y + x];
+
+            if c.is_ascii_digit() {
+                found_digit = true;
+                numbers[y] = numbers[y] * 10 + u64::from(c - b'0');
+            }
+        }
+
+        if !found_digit {
+            match op {
+                b'+' => {
+                    let mut total = 0;
+                    for number in numbers.iter_mut().take(number_of_rows - 1) {
+                        total += *number;
+                        *number = 0;
+                    }
+                    grand_total += total;
+                }
+                b'*' => {
+                    let mut total = 1;
+                    for number in numbers.iter_mut().take(number_of_rows - 1) {
+                        total *= *number;
+                        *number = 0;
+                    }
+                    grand_total += total;
+                }
+                _ => unreachable!(),
+            }
+        }
+
+        if ops[x] != b' ' {
+            op = ops[x];
+        }
+    }
+
+    match op {
+        b'+' => {
+            let mut total = 0;
+            for number in numbers.iter_mut().take(number_of_rows - 1) {
+                total += *number;
+                *number = 0;
+            }
+            grand_total += total;
+        }
+        b'*' => {
+            let mut total = 1;
+            for number in numbers.iter_mut().take(number_of_rows - 1) {
+                total *= *number;
+                *number = 0;
+            }
+            grand_total += total;
+        }
+        _ => unreachable!(),
+    }
+
+    grand_total
 }
 
 #[cfg(test)]
@@ -15,8 +84,8 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let result = solve(b"");
-        assert_eq!(result, 0);
+        let result = solve(b"123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  ");
+        assert_eq!(result, 4_277_556);
     }
 }
 
