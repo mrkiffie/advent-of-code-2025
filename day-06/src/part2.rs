988d2296795cd3cfe83c4dc167c7babdaf174734
mrkiffie <mrkiffie@gmail.com>
2025-12-06 13:30:36 +0200

    Solve day-06 part2

--- day-06/src/part2.rs
+++ day-06/src/part2.rs
@@ -5,8 +5,55 @@ pub fn main() {
     println!("{result}");
 }
 
-fn solve(input: &[u8]) -> i32 {
-    0
+fn solve(input: &[u8]) -> u64 {
+    let row_length = input.iter().position(|c| *c == b'\n').unwrap();
+    let number_of_rows = input.len() / row_length;
+
+    let operator_index = (number_of_rows - 1) * (row_length + 1);
+    let ops = &input[operator_index..operator_index + row_length];
+
+    let mut grand_total = 0;
+    let mut op = b' ';
+    let mut total = 0;
+
+    for x in 0..row_length {
+        if ops[x] != b' ' {
+            op = ops[x];
+        }
+        let mut found_digit = false;
+        let mut number = 0;
+        for y in 0..number_of_rows - 1 {
+            let c = input[(row_length + 1) * y + x];
+
+            if c.is_ascii_digit() {
+                found_digit = true;
+                number = number * 10 + u64::from(c - b'0');
+            }
+        }
+
+        if found_digit {
+            match op {
+                b'+' => {
+                    total += number;
+                }
+                b'*' => {
+                    if total == 0 {
+                        total = number;
+                    } else {
+                        total *= number;
+                    }
+                }
+                _ => unreachable!(),
+            }
+        }
+
+        if !found_digit {
+            grand_total += total;
+            total = 0;
+        }
+    }
+    grand_total += total;
+    grand_total
 }
 
 #[cfg(test)]
@@ -15,8 +62,8 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let result = solve(b"");
-        assert_eq!(result, 0);
+        let result = solve(b"123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  ");
+        assert_eq!(result, 3_263_827);
     }
 }
 
