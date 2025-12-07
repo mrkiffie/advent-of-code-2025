e8eba675ec8bdc48b143612ffe6953b51c0dea9c
mrkiffie <mrkiffie@gmail.com>
2025-12-07 17:16:00 +0200

    Refactor day-7 part1

--- day-07/src/part1.rs
+++ day-07/src/part1.rs
@@ -1,79 +1,44 @@
-use std::collections::{HashMap, HashSet, VecDeque};
-
-const INPUT: &str = include_str!("./input.txt");
+const INPUT: &[u8] = include_bytes!("./input.txt");
 
 pub fn main() {
     let result = solve(INPUT);
     println!("{result}");
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct IVec2 {
-    x: i32,
-    y: i32,
-}
+const INPUT_ROW_WIDTH: usize = 141;
 
-#[allow(clippy::cast_possible_truncation)]
-#[allow(clippy::cast_possible_wrap)]
-fn solve(input: &str) -> usize {
-    let mut lookup = HashMap::<IVec2, bool>::with_capacity(1792);
-    let mut start = IVec2 { x: 0, y: 0 };
-    for (y, line) in input.trim().lines().enumerate() {
-        for (x, c) in line.chars().enumerate() {
-            match c {
-                'S' => {
-                    start = IVec2 {
-                        x: x as i32,
-                        y: y as i32,
-                    };
-                }
-                '^' => {
-                    lookup.insert(
-                        IVec2 {
-                            x: x as i32,
-                            y: y as i32,
-                        },
-                        false,
-                    );
-                }
-                _ => {}
+fn solve(input: &[u8]) -> usize {
+    let mut previous = [0usize; INPUT_ROW_WIDTH];
+    let mut current = [0usize; INPUT_ROW_WIDTH];
+    let mut i = 0;
+    let mut count = 0;
+    for c in input {
+        match c {
+            b'\n' => {
+                i = 0;
+                previous.copy_from_slice(&current);
             }
+            b'.' => {
+                i += 1;
+            }
+            b'S' => {
+                current[i] = 1;
+                i += 1;
+            }
+            b'^' => {
+                debug_assert!(i > 0);
+                if previous[i] != 0 {
+                    count += 1;
+                }
+                current[i] = 0;
+                current[i - 1] += previous[i];
+                current[i + 1] += previous[i];
+                i += 1;
+            }
+            _ => unreachable!(),
         }
     }
-
-    let max = lookup.keys().max_by(|a, b| a.y.cmp(&b.y)).unwrap().y;
-    let mut visited = HashSet::<IVec2>::with_capacity(3584);
-    let mut queue = VecDeque::with_capacity(128);
-    queue.push_back(start);
-    while let Some(mut beam) = queue.pop_front() {
-        if visited.contains(&beam) {
-            continue;
-        }
-        visited.insert(beam);
-        while beam.y <= max {
-            beam.y += 1;
-            if let Some(splitter) = lookup.get_mut(&beam) {
-                *splitter = true;
-                let left = IVec2 {
-                    x: beam.x - 1,
-                    y: beam.y,
-                };
-                if !visited.contains(&left) {
-                    queue.push_back(left);
-                }
-                let right = IVec2 {
-                    x: beam.x + 1,
-                    y: beam.y,
-                };
-                if !visited.contains(&right) {
-                    queue.push_back(right);
-                }
-
-                break;
-            }
-        }
-    }
-    lookup.values().filter(|x| **x).count()
+    count
 }
 
 #[cfg(test)]
@@ -83,7 +48,7 @@ mod tests {
     #[test]
     fn test_1() {
         let result = solve(
-            ".......S.......\n...............\n.......^.......\n...............\n......^.^......\n...............\n.....^.^.^.....\n...............\n....^.^...^....\n...............\n...^.^...^.^...\n...............\n..^...^.....^..\n...............\n.^.^.^.^.^...^.\n...............",
+            b".......S.......\n...............\n.......^.......\n...............\n......^.^......\n...............\n.....^.^.^.....\n...............\n....^.^...^....\n...............\n...^.^...^.^...\n...............\n..^...^.....^..\n...............\n.^.^.^.^.^...^.\n...............",
         );
         assert_eq!(result, 21);
     }
