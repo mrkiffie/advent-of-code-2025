d29cfb3aacdeaf73a5856d48c452edb0d6cac7ec
mrkiffie <mrkiffie@gmail.com>
2025-12-07 07:19:27 +0200

    Generate day-07

---
+++ day-07/src/bin/part1.rs
@@ -0,0 +1,10 @@
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
+fn main() {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
+    day_07::part1::main();
+}
