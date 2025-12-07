cb6da530f736eecc51beab47204b003ed94c0dac
mrkiffie <mrkiffie@gmail.com>
2025-12-07 17:16:20 +0200

    Solve day-07 part2

--- day-07/src/part2.rs
+++ day-07/src/part2.rs
@@ -5,8 +5,36 @@ pub fn main() {
     println!("{result}");
 }
 
-fn solve(input: &[u8]) -> i32 {
-    0
+const INPUT_ROW_WIDTH: usize = 141;
+
+fn solve(input: &[u8]) -> usize {
+    let mut previous = [0usize; INPUT_ROW_WIDTH];
+    let mut current = [0usize; INPUT_ROW_WIDTH];
+    let mut i = 0;
+    for c in input {
+        match c {
+            b'\n' => {
+                i = 0;
+                previous.copy_from_slice(&current);
+            }
+            b'.' => {
+                i += 1;
+            }
+            b'S' => {
+                current[i] = 1;
+                i += 1;
+            }
+            b'^' => {
+                debug_assert!(i > 0);
+                current[i] = 0;
+                current[i - 1] += previous[i];
+                current[i + 1] += previous[i];
+                i += 1;
+            }
+            _ => unreachable!(),
+        }
+    }
+    current.iter().sum()
 }
 
 #[cfg(test)]
@@ -15,8 +43,10 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let result = solve(b"");
-        assert_eq!(result, 0);
+        let result = solve(
+            b".......S.......\n...............\n.......^.......\n...............\n......^.^......\n...............\n.....^.^.^.....\n...............\n....^.^...^....\n...............\n...^.^...^.^...\n...............\n..^...^.....^..\n...............\n.^.^.^.^.^...^.\n...............",
+        );
+        assert_eq!(result, 40);
     }
 }
 
