433332842817071bffe3f3691cd88234f0bd3661
mrkiffie <mrkiffie@gmail.com>
2025-12-09 08:10:45 +0200

    Solve day-09 part1

--- day-09/src/part1.rs
+++ day-09/src/part1.rs
@@ -5,8 +5,52 @@ pub fn main() {
     println!("{result}");
 }
 
-fn solve(input: &[u8]) -> i32 {
-    0
+#[derive(Debug, Copy, Clone)]
+struct IVec2 {
+    x: i32,
+    y: i32,
+}
+
+impl IVec2 {
+    fn area(self, rhs: IVec2) -> i64 {
+        i64::from((self.x - rhs.x).abs() + 1) * i64::from((self.y - rhs.y).abs() + 1)
+    }
+}
+
+fn solve(input: &[u8]) -> i64 {
+    let mut coordinates = Vec::new();
+
+    let mut value: i32 = 0;
+    let mut x = 0;
+
+    for b in input {
+        match b {
+            b'0'..=b'9' => value = value * 10 + i32::from(b - b'0'),
+            b',' => {
+                x = value;
+                value = 0;
+            }
+            b'\n' => {
+                coordinates.push(IVec2 { x, y: value });
+                value = 0;
+                x = 0;
+            }
+            _ => {
+                unreachable!();
+            }
+        }
+    }
+
+    let mut max_area = 0;
+
+    for (i, a) in coordinates.iter().enumerate() {
+        for b in coordinates.iter().skip(i + 1) {
+            if a.area(*b) > max_area {
+                max_area = a.area(*b);
+            }
+        }
+    }
+    max_area
 }
 
 #[cfg(test)]
@@ -15,8 +59,8 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let result = solve(b"");
-        assert_eq!(result, 0);
+        let result = solve(b"7,1\n11,1\n11,7\n9,7\n9,5\n2,5\n2,3\n7,3\n");
+        assert_eq!(result, 50);
     }
 }
 
