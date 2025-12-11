df5481faebe38da4336af17868c2f82422b31a64
mrkiffie <mrkiffie@gmail.com>
2025-12-11 15:29:53 +0200

    Solve day-11 part1

--- day-11/src/part1.rs
+++ day-11/src/part1.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, VecDeque};
+
 const INPUT: &[u8] = include_bytes!("./input.txt");
 
 pub fn main() {
@@ -6,7 +8,54 @@ pub fn main() {
 }
 
 fn solve(input: &[u8]) -> i32 {
-    0
+    type Id = [u8; 3];
+    assert_eq!(input.last(), Some(&b'\n'));
+
+    let mut map: HashMap<Id, Vec<Id>> = HashMap::with_capacity(768);
+
+    let mut key = [0; 3];
+    let mut val = [0; 3];
+    let mut i = 0;
+
+    for b in input {
+        match b {
+            b'a'..=b'z' => {
+                val[i] = *b;
+                i += 1;
+            }
+            b':' => {
+                key = val;
+                i = 0;
+            }
+            b'\n' | b' ' => {
+                if key != val {
+                    map.entry(key)
+                        .and_modify(|entry| entry.push(val))
+                        .or_insert(vec![val]);
+                }
+                i = 0;
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    let mut queue = VecDeque::with_capacity(768);
+    queue.push_back(b"you");
+
+    let mut count = 0;
+    while let Some(key) = queue.pop_front() {
+        if let Some(branches) = map.get(key) {
+            for branch in branches {
+                if branch == b"out" {
+                    count += 1;
+                } else {
+                    queue.push_back(branch);
+                }
+            }
+        }
+    }
+
+    count
 }
 
 #[cfg(test)]
@@ -15,8 +64,10 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let result = solve(b"");
-        assert_eq!(result, 0);
+        let result = solve(
+            b"aaa: you hhh\nyou: bbb ccc\nbbb: ddd eee\nccc: ddd eee fff\nddd: ggg\neee: out\nfff: out\nggg: out\nhhh: ccc fff iii\niii: out\n",
+        );
+        assert_eq!(result, 5);
     }
 }
 
